b2295b31791591ebe56ea05e793800adc5d0ac9b
MohammedALREAI <mhamad.aa1997.aa@gmail.com>
2021-08-25 02:21:59 +0300

    made  some  componet

---
+++ src/components/CustomMenu/Menu/components/Boxs/Index.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+import { Box, Button, Flex, Link, Overlay, Text, useMatchBreakpoints } from "@pancakeswap/uikit";
+
+export const HeaderButtonIcon=styled(Button)`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 8px;
+width: 40px;
+max-height: 40px;
+height: 40px;
+
+margin: 0px  4.2px;
+background: #3B3C4E;
+border-radius: 12px;
+
+
+`;
